9aae70eccbc825c54e2bc78b4e237045f33c230e
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2025-12-11 00:10:45 +0100

    test cors

--- back/src/app.ts
+++ back/src/app.ts
@@ -6,9 +6,7 @@ import { getStats } from './controllers/stats.js'
 export class App {
   static createApp() {
     const app = new Hono().basePath('/api')
-    app.use('*', (c, next) => {
-      return cors({ origin: c.var.FRONT_URL })(c, next)
-    })
+    app.use('*', cors())
     app.get('/players', getPlayers)
     app.get('/players/:id', getPlayer)
     app.post('/players', postPlayer)
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: { proxy: { '/api': 'http://localhost:3000' } },
+  server: { proxy: { '/api': 'http://localhost:8787' } },
   plugins: [
     tailwindcss(),
     react({
