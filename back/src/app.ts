d8fa02a7522f8c70aa5ba75f3168d7cb141a1fca
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2025-12-11 00:19:39 +0100

    test cors

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,11 +1,12 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { getPlayer, getPlayers, postPlayer } from './controllers/players.js'
 import { getStats } from './controllers/stats.js'
 
 export class App {
   static createApp() {
     const app = new Hono().basePath('/api')
-    //app.use('*', cors())
+    app.use('*', cors({ origin: '*' }))
     app.get('/players', getPlayers)
     app.get('/players/:id', getPlayer)
     app.post('/players', postPlayer)
