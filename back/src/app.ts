623af2d589af45b8a83e546153dbe46649135d5e
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2025-12-11 12:08:12 +0100

    test

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,12 +1,18 @@
 import { type Context, Hono } from 'hono'
 import { getPlayer, getPlayers, postPlayer } from './controllers/players.js'
 import { getStats } from './controllers/stats.js'
-import { corsMiddleware } from './middlewares/cors.js'
+import { cors } from 'hono/cors'
 
 export class App {
   static createApp() {
     const app = new Hono().basePath('/api')
-    app.use('*', corsMiddleware)
+    app.use('/api/*', (c: Context, next) => {
+      return cors({
+        origin: c.env.FRONT_URL,
+        allowMethods: ['GET', 'POST'],
+        allowHeaders: ['Content-Type'],
+      })(c, next)
+    })
     app.get('/players', getPlayers)
     app.get('/players/:id', getPlayer)
     app.post('/players', postPlayer)
