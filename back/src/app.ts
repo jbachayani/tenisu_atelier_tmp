71c13a682e7edb91f92a55538090dbf1920980d6
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2025-12-11 14:42:59 +0100

    use middleware

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,18 +1,12 @@
 import { type Context, Hono } from 'hono'
 import { getPlayer, getPlayers, postPlayer } from './controllers/players.js'
 import { getStats } from './controllers/stats.js'
-import { cors } from 'hono/cors'
+import { corsMiddleware } from './middlewares/cors.js'
 
 export class App {
   static createApp() {
     const app = new Hono().basePath('/api')
-    app.use('*', (c: Context, next) => {
-      return cors({
-        origin: c.env.FRONT_URL,
-        allowMethods: ['GET', 'POST'],
-        allowHeaders: ['Content-Type'],
-      })(c, next)
-    })
+    app.use('*', corsMiddleware)
     app.get('/players', getPlayers)
     app.get('/players/:id', getPlayer)
     app.post('/players', postPlayer)
