188f06debd6cf618ad6b7fea2aeefc87dab2e716
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2025-12-11 15:29:52 +0100

    patch: fix unit tests

--- back/tests/controller/players.test.ts
+++ back/tests/controller/players.test.ts
@@ -1,14 +1,18 @@
 import { describe, expect, it, vi } from 'vitest'
 import { App } from '../../src/app.js'
 import { mockPlayers } from '../mock.js'
-import { players } from '../../src/services.js'
 
-vi.mock('fs/promises')
-vi.mock('../src/services.js', () => ({ players: mockPlayers }))
+const mockEnv = () => ({
+  TENISU_DB: {
+    get: vi.fn().mockImplementation(() => JSON.stringify(mockPlayers)),
+    put: vi.fn(),
+  },
+  FRONT_URL: 'http://url',
+})
 
 describe('getPlayers', () => {
   it('should return all players sorted', async () => {
-    const res = await App.createApp().request('/api/players')
+    const res = await App.createApp().request('/api/players', {}, mockEnv())
     expect(res.status).toBe(200)
     await expect(res.json()).resolves.toMatchSnapshot()
   })
@@ -16,13 +20,13 @@ describe('getPlayers', () => {
 
 describe('getPlayer', () => {
   it('should return a player by id', async () => {
-    const res = await App.createApp().request('/api/players/17')
+    const res = await App.createApp().request('/api/players/17', {}, mockEnv())
     expect(res.status).toBe(200)
     await expect(res.json()).resolves.toEqual([mockPlayers[0]])
   })
 
   it('should return 400 when id is not a number', async () => {
-    const res = await App.createApp().request('/api/players/ohNooo')
+    const res = await App.createApp().request('/api/players/ohNooo', {}, mockEnv())
     expect(res.status).toBe(400)
     await expect(res.json()).resolves.toEqual({
       error: [
@@ -38,10 +42,9 @@ describe('getPlayer', () => {
   })
 
   it('should return 500 when an internal error', async () => {
-    vi.spyOn(players, 'filter').mockImplementation(() => {
-      throw new Error('Unknown error')
-    })
-    const res = await App.createApp().request('/api/players/17')
+    const env = mockEnv()
+    env.TENISU_DB.get = vi.fn().mockRejectedValue(new Error('Unknown error'))
+    const res = await App.createApp().request('/api/players/17', {}, env)
     expect(res.status).toBe(500)
     await expect(res.json()).resolves.toEqual({ error: 'Unknown error' })
   })
