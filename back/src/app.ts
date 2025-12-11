261c5794ae94c28abf6e968d4cadf03bc7e887db
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2025-12-11 11:50:03 +0100

    fix cors test local

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,24 +1,12 @@
 import { type Context, Hono } from 'hono'
 import { getPlayer, getPlayers, postPlayer } from './controllers/players.js'
 import { getStats } from './controllers/stats.js'
-import { cors } from 'hono/cors'
+import { corsMiddleware } from './middlewares/cors.js'
 
 export class App {
   static createApp() {
     const app = new Hono().basePath('/api')
-    app.use(
-      '*',
-      cors({
-        // on renvoie toujours FRONT_URL, peu importe l’Origin de la requête
-        origin: (origin, c: Context) => {
-          console.log('🌐 Origin reçu:', origin)
-          console.log('🎯 FRONT_URL binding:', c.env.FRONT_URL)
-          return c.env.FRONT_URL
-        },
-        allowMethods: ['GET', 'POST', 'OPTIONS'],
-        allowHeaders: ['Content-Type'],
-      })
-    )
+    app.use('*', corsMiddleware)
     app.get('/players', getPlayers)
     app.get('/players/:id', getPlayer)
     app.post('/players', postPlayer)
